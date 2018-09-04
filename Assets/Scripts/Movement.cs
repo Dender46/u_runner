4ea57e4f4e13de80949f69a56e55eadb2b425065
Dmitriy Bagandov <dimon.bagandov@gmail.com>
2018-09-04 21:17:22 +0300

    init commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public Rigidbody player;
+
+	public float forwardForce = 2000f;
+	public float sideForce = 500f;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		player.AddForce (0, 0, forwardForce * Time.deltaTime);
+		if (Input.GetKey ("a"))
+			player.AddForce (-sideForce * Time.deltaTime, 0, 0);
+		if (Input.GetKey("d"))
+			player.AddForce(sideForce * Time.deltaTime, 0, 0);
+	}
+}
---
+++ Assets/Scripts/OnCollision.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class OnCollision : MonoBehaviour {
+
+	public Movement movement;
+	public Text scoreText;
+
+	// Update is called once per frame
+	void OnCollisionEnter (Collision colInfo) {
+		if (colInfo.collider.tag == "Obstacle") {
+			movement.enabled = false;
+			scoreText.text = "game over";
+			Debug.Log ("", scoreText);
+		}
+	}
+}
